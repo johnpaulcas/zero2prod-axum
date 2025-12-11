c9e74807649509575610a130bb8294428fab8905
johnpaulcas <casjp.app@gmail.com>
2025-12-11 19:47:17 +0800

    ✨ impl rust runner

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use zero2prod_axum::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    run(listener).await
 }
