beeadd040e1fb3bbcf0f4ce12c4640ee3c654923
johnpaulcas <casjp.app@gmail.com>
2025-12-11 19:47:39 +0800

    ✨ add initial subscription

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+use axum::{
+    Form, Router,
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+};
+use tokio::net::TcpListener;
+
+#[derive(serde::Deserialize, Debug)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn health_check() -> StatusCode {
+    StatusCode::OK
+}
+
+async fn subscribe(Form(_form): Form<FormData>) -> impl IntoResponse {
+    println!("{:?}", _form);
+    StatusCode::OK
+}
+
+pub fn app() -> Router {
+    Router::new().route("/health-check", get(health_check))
+}
+
+pub async fn run(listener: TcpListener) -> Result<(), std::io::Error> {
+    let app = Router::new()
+        .route("/health-check", get(health_check))
+        .route("/subscription", post(subscribe));
+
+    axum::serve(listener, app).await
+}
