6deb20b47aabf915eb339c493ca10d8c7ad1d3fa
johnpaulcas <casjp.app@gmail.com>
2025-12-13 08:08:18 +0800

    ♻️ add telemetry

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
-use zero2prod_axum::{configuration::get_configuration, run};
+use sqlx::PgPool;
+use zero2prod_axum::{configuration::get_configuration, startup::run, telemetry::init_telemetry};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let config = get_configuration().expect("Failed to load configuration");
+    init_telemetry("zero2prod".into(), "info".into());
 
+    let config = get_configuration().expect("Failed to load configuration");
     let address = format!("127.0.0.1:{}", config.application_port);
 
+    let pg_pool = PgPool::connect(&config.database.connection_string())
+        .await
+        .expect("Failed connecting to database");
+
     let listener = tokio::net::TcpListener::bind(address).await?;
-    run(listener).await
+    run(listener, pg_pool).await?;
+
+    Ok(())
 }
