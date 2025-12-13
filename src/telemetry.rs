64be99165670102a7c222de7199ef2d3156bfde0
johnpaulcas <casjp.app@gmail.com>
2025-12-14 00:59:24 +0800

    ✨ add sink support

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -1,12 +1,15 @@
 use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_subscriber::{EnvFilter, Registry, layer::SubscriberExt};
+use tracing_subscriber::{EnvFilter, Registry, fmt::MakeWriter, layer::SubscriberExt};
 
-pub fn init_telemetry(name: String, env_filter: String) {
+pub fn init_telemetry<Sink>(name: String, env_filter: String, sink: Sink)
+where
+    Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static,
+{
     let env_filter =
         EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
 
-    let formatter_layer = BunyanFormattingLayer::new(name, std::io::stdout);
+    let formatter_layer = BunyanFormattingLayer::new(name, sink);
 
     let subscriber = Registry::default()
         .with(env_filter)
