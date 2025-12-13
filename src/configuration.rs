ba3d87f4301aa24800e76132343c6c6acdc8d89e
johnpaulcas <casjp.app@gmail.com>
2025-12-14 00:59:43 +0800

    ✨ add secrecy

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,3 +1,4 @@
+use secrecy::{ExposeSecret, Secret};
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -6,10 +7,10 @@ pub struct Settings {
     pub application_port: u16,
 }
 
-#[derive(Clone, Deserialize)]
+#[derive(Deserialize, Clone)]
 pub struct DatabaseSettings {
     pub username: String,
-    pub password: String,
+    pub password: Secret<String>,
     pub port: u16,
     pub host: String,
     pub database_name: String,
@@ -19,7 +20,11 @@ impl DatabaseSettings {
     pub fn connection_string(&self) -> String {
         format!(
             "postgres://{}:{}@{}:{}/{}",
-            self.username, self.password, self.host, self.port, self.database_name
+            self.username,
+            self.password.expose_secret(),
+            self.host,
+            self.port,
+            self.database_name
         )
     }
 }
