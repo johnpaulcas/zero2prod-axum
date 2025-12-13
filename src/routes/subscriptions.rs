6520c7034cede53433455bdb72db6adf84740232
johnpaulcas <casjp.app@gmail.com>
2025-12-13 08:07:48 +0800

    ✨ add tracing to subscriptions

---
+++ src/routes/subscriptions.rs
@@ -0,0 +1,59 @@
+use std::os::macos::raw::stat;
+use std::sync::Arc;
+
+use axum::{Form, extract::State, http::StatusCode, response::IntoResponse};
+use chrono::Utc;
+use sqlx::PgPool;
+use tracing::Instrument;
+use tracing::error;
+use tracing::info;
+use uuid::Uuid;
+
+use crate::startup::AppState;
+
+#[derive(serde::Deserialize, Debug)]
+pub struct FormData {
+    email: String,
+    name: String,
+}
+
+#[tracing::instrument(
+    name = "Adding new subscriber",
+    skip_all,
+    fields(
+        request_id = %Uuid::new_v4(),
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    )
+)]
+pub async fn subscribe(
+    State(state): State<Arc<AppState>>,
+    Form(form): Form<FormData>,
+) -> impl IntoResponse {
+    match insert_subscriber(&state.pg_pool, &form).await {
+        Ok(_) => StatusCode::OK,
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+    }
+}
+
+#[tracing::instrument(name = "Saving new subscriber detalis to the database", skip_all)]
+pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, name, email, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+    "#,
+        Uuid::new_v4(),
+        form.name,
+        form.email,
+        Utc::now()
+    )
+    .execute(pool)
+    .await
+    .map_err(|e| {
+        error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+
+    Ok(())
+}
