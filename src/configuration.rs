5a0892c5f9a766b091162dd6f42c8db62765c882
johnpaulcas <casjp.app@gmail.com>
2025-12-14 10:40:43 +0800

    ♻️ load env base on the app configuration

--- src/configuration.rs
+++ src/configuration.rs
@@ -4,7 +4,13 @@ use serde::Deserialize;
 #[derive(Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16,
+    pub application: ApplicationSettings,
+}
+
+#[derive(Deserialize)]
+pub struct ApplicationSettings {
+    pub port: u16,
+    pub host: String,
 }
 
 #[derive(Deserialize, Clone)]
@@ -30,9 +36,52 @@ impl DatabaseSettings {
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let base_path = std::env::current_dir().expect("Failed to get current directory");
+    let configuration_dir = base_path.join("configuration");
+
+    // get the current env
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT");
+
+    let environment_filename = format!("{}.yaml", environment.as_str());
+
     let settings = config::Config::builder()
-        .add_source(config::File::with_name("configuration"))
+        .add_source(config::File::from(configuration_dir.join("base.yaml")))
+        .add_source(config::File::from(
+            configuration_dir.join(environment_filename),
+        ))
         .build()?;
 
     settings.try_deserialize::<Settings>()
+}
+
+pub enum Environment {
+    Local,
+    Production,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Self::Local => "local",
+            Self::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "Unable to convert {}, value must `local` or `production`",
+                other
+            )),
+        }
+    }
 }
