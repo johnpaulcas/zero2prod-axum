d96e767760657a1af37e97bd138df0eae3aaaaaa
johnpaulcas <casjp.app@gmail.com>
2025-12-14 22:37:25 +0800

    ✨ add SubscriberName parsing

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -4,9 +4,13 @@ use axum::{Form, extract::State, http::StatusCode, response::IntoResponse};
 use chrono::Utc;
 use sqlx::PgPool;
 use tracing::error;
+use unicode_segmentation::UnicodeSegmentation;
 use uuid::Uuid;
 
-use crate::startup::AppState;
+use crate::{
+    domain::{NewSubscriber, SubsciberName},
+    startup::AppState,
+};
 
 #[derive(serde::Deserialize, Debug)]
 pub struct FormData {
@@ -26,21 +30,31 @@ pub async fn subscribe(
     State(state): State<Arc<AppState>>,
     Form(form): Form<FormData>,
 ) -> impl IntoResponse {
-    match insert_subscriber(&state.pg_pool, &form).await {
+    let name = match SubsciberName::parse(form.name) {
+        Ok(name) => name,
+        Err(_) => return StatusCode::BAD_REQUEST,
+    };
+
+    let new_subsriber = NewSubscriber {
+        email: form.email,
+        name,
+    };
+
+    match insert_subscriber(&state.pg_pool, &new_subsriber).await {
         Ok(_) => StatusCode::OK,
         Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
     }
 }
 
 #[tracing::instrument(name = "Saving new subscriber detalis to the database", skip_all)]
-pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+pub async fn insert_subscriber(pool: &PgPool, form: &NewSubscriber) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, name, email, subscribed_at)
         VALUES ($1, $2, $3, $4)
     "#,
         Uuid::new_v4(),
-        form.name,
+        form.name.as_ref(),
         form.email,
         Utc::now()
     )
